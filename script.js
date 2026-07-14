9eb06f4dc5cc55b77d94578de24d15cdafecb13d
Mahama59 <113291429+Mahama59@users.noreply.github.com>
2026-07-14 15:55:36 +0000

    Implement product search and add to cart functions

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+
+let cart = [];
+
 function searchProducts() {
+
     let input = document.querySelector("input").value.toLowerCase();
 
     let products = document.querySelectorAll(".product");
 
     products.forEach(function(product) {
+
         let text = product.innerText.toLowerCase();
 
         if (text.includes(input)) {
@@ -11,5 +16,18 @@ function searchProducts() {
         } else {
             product.style.display = "none";
         }
+
     });
+
+}
+
+
+function addToCart(productName) {
+
+    cart.push(productName);
+
+    alert(productName + " added to cart!");
+
+    console.log(cart);
+
 }
