e36c62070f31873e4dc3f7ddc8fe2969d2905b23
Mahama59 <113291429+Mahama59@users.noreply.github.com>
2026-07-18 10:36:33 +0000

    Create app.js

---
+++ web/app.js
@@ -0,0 +1,12 @@
+// ================= PRODUCT FINDER APP =================
+
+console.log("Product Finder App Started");
+
+
+document.addEventListener(
+"DOMContentLoaded",
+function(){
+
+    console.log("Page Loaded Successfully");
+
+});
