c788d2b99c65969dfda0a9baa2d443ea07107902
Mahama59 <113291429+Mahama59@users.noreply.github.com>
2026-07-21 13:19:16 +0000

    Refactor completeOrder function for clarity

--- js/products.js
+++ js/products.js
@@ -233,75 +233,73 @@ function checkout(){
 
 function completeOrder(){
 
-    let orders =
-    JSON.parse(localStorage.getItem("orders")) || [];
+let cart =
+JSON.parse(localStorage.getItem("cart")) || [];
 
 
-    let cart =
-    JSON.parse(localStorage.getItem("cart")) || [];
+if(cart.length === 0){
 
-
-    if(cart.length === 0){
-
-        alert("Your cart is empty");
-        return;
-
-    }
-
-
-    let user =
-    JSON.parse(localStorage.getItem("user")) || {
-        name:"Guest"
-    };
-
-
-    let order = {
-
-        id: Date.now(),
-
-        customer:user.name,
-
-        items:cart,
-
-        total:cart.reduce(
-            function(sum,item){
-
-                return sum + (item.price * item.quantity);
-
-            },0
-        ),
-
-        status:"New",
-
-        date:new Date().toLocaleString()
-
-    };
-
-
-    orders.push(order);
-
-
-    localStorage.setItem(
-        "orders",
-        JSON.stringify(orders)
-    );
-
-
-    console.log("ORDER SAVED:", order);
-
-
-    // empty cart
-
-    localStorage.removeItem("cart");
-
-
-    alert("Order completed successfully");
-
-
-    window.location.href="success.html";
+alert("Your cart is empty");
+return;
 
 }
 
+
+let orders =
+JSON.parse(localStorage.getItem("orders")) || [];
+
+
+let user =
+JSON.parse(localStorage.getItem("user")) || {
+name:"Guest"
+};
+
+
+
+let order = {
+
+id: Date.now(),
+
+customer:user.name,
+
+items:cart,
+
+total:cart.reduce(
+(sum,item)=>sum + (item.price * item.quantity),
+0
+),
+
+status:"New",
+
+date:new Date().toLocaleString()
+
+};
+
+
+
+orders.push(order);
+
+
+
+localStorage.setItem(
+"orders",
+JSON.stringify(orders)
+);
+
+
+
+localStorage.removeItem("cart");
+
+
+
+alert("Order placed successfully");
+
+
+
+window.location.href="success.html";
+
+
+}
 // ================= PRODUCT VIEW =================
 function viewProduct(name){
 
